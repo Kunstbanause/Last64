2dd8470cfffea49e68b8d69664c4b143e6d06d03
Kunstbanause <kunstbanausen@googlemail.com>
2025-09-04 20:36:58 +0200

    Color converter

--- last64_bloom/src/render/colors.h
+++ last64_bloom/src/render/colors.h
@@ -21,6 +21,31 @@ namespace Colors {
         0x000000FF, // Black
         0x800000FF, // Maroon
     };
+
+    const uint32_t moreColors[] = {
+        0x85DAEBFF, // #85daeb
+        0x5FC9E7FF, // #5fc9e7
+        0x5FA1E7FF, // #5fa1e7
+        0x5F6EE7FF, // #5f6ee7
+        0x4C60AAFF, // #4c60aa
+        0x444774FF, // #444774
+        0x32313BFF, // #32313b
+        0x463C5EFF, // #463c5e
+        0x5D4776FF, // #5d4776
+        0x855395FF, // #855395
+        0xAB58A8FF, // #ab58a8
+        0xCA60AEFF, // #ca60ae
+        0xF3A787FF, // #f3a787
+        0xF5DAA7FF, // #f5daa7
+        0x8DD894FF, // #8dd894
+        0x5DC190FF, // #5dc190
+        0x4AB9A3FF, // #4ab9a3
+        0x4593A5FF, // #4593a5
+        0x5EFDF7FF, // #5efdf7
+        0xFF5DCCFF, // #ff5dcc
+        0xFDFE89FF, // #fdfe89
+        0xFFFFFFFF, // #ffffff
+    };
 }
 
 #endif // LAST64_BLOOM_COLORS_H
